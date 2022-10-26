b1d908ff13b926438696209a9dadff9212391af8
Anastasiya Olhovik <93829935+AnastasiyaOlhovik@users.noreply.github.com>
2022-10-26 06:30:26 +0300

    +img

--- src/components/ErrorSearch/ErrorSearch.js
+++ src/components/ErrorSearch/ErrorSearch.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ErrorSearch.module.css';
+import errorImg from 'img/error.png';
 
 export default function ErrorSearch({ message }) {
     return (
-        <div className={styles.errorWrapper} role="alert">
+        <div className={styles.ErrorWrapper} role="alert">
             <p>{message}</p>
+            <img src={errorImg} alt="empty" />
         </div>
     );
 }
